2a11144739b548ec12e104f9ced5a0dd441b51a1
Koray Ozkal <koryozkal@gmail.com>
2020-05-03 01:12:05 +0000

    Automatically backed up by Learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -10,4 +10,17 @@ class Genre
   def self.all
     @@all
   end
+  
+  def songs
+    Song.all.select {|song| song.genre == self}
+  end
+
+  def artists
+    self.songs.map { |song| song.artist }
+  end
+
+  def self.all
+    @@all
+  end
+end
 end
