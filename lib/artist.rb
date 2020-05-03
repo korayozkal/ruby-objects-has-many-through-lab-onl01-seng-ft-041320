c503fa82c88e49e4603c1cf854f43718994de2d4
Koray Ozkal <koryozkal@gmail.com>
2020-05-03 00:56:27 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -15,10 +15,9 @@ end
 def songs(name)
   Song.all.select  {|song| song.artist == self}
 end
-
 def new_song(name, genre)
-  Song.new(name,genre)
-  
+  song = Song.new(name, self, genre)
+  end
   
   
 end 
