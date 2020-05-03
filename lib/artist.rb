54a4c0d08cdf9f1423f66c6529386a005e91dcfc
Koray Ozkal <koryozkal@gmail.com>
2020-05-03 01:16:49 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -6,17 +6,20 @@ class Artist
     @name = name
     @@all << self
   end
-  
-  def self.all
-    @@all
-  end
-  
+
   def songs
     Song.all.select {|song| song.artist == self }
   end
-  
+
+  def new_song name, genre
+    song = Song.new(name, self, genre)
+  end
+
   def genres
     self.songs.map {|song| song.genre }
   end
 
+  def self.all
+    @@all
+  end
 end
